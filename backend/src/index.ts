753a782ffa539e546af0c727651bcbf670c47de2
Timothy <dinosauli2006@gmail.com>
2025-09-06 20:44:17 -0400

    feat add vercel.json for frontend deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,8 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173',"https://expense-splitter-jade.vercel.app/login"],
+  // for using cookies
   credentials: true
 }));
 
